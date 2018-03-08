0c6d6401560fb7dc76d54b324882747786753b02
EliteSpud <37165449+EliteSpud@users.noreply.github.com>
2018-03-08 04:07:43 +0000

    3
    First attempt at image icons on buttons
    
    Didn't work

--- SlidePuzzle.java
+++ SlidePuzzle.java
@@ -34,6 +34,7 @@ public class SlidePuzzle extends JFrame implements ActionListener
 				buttons[count] = new JButton();
 				buttons[count].setSize(100,100);
 				buttons[count].setLocation(x,y);
+				buttons[count].addActionListener(this);
 				pnl.add(buttons[count]);
 				x += 100;
 				count++;
@@ -41,10 +42,69 @@ public class SlidePuzzle extends JFrame implements ActionListener
 			x = 0;
 			y += 100;
 		}
+		loadIcons();
+	}
+	public void loadIcons()
+	{
+		ImageIcon iconArray[] = new ImageIcon[12];
+		for(int i = 0;i<iconArray.length; i++)
+		{
+			String currentFile = ("bart" + String.valueOf(i));
+			System.out.println("Current file : "+currentFile);
+			iconArray[i] = new ImageIcon(currentFile);
+			buttons[i].setIcon(iconArray[i]);
+		}
 	}
 	public void actionPerformed(ActionEvent e)
 	{
-		
+		if(e.getSource() == buttons[0])
+		{
+			System.out.println("Button 0");
+		}
+		if(e.getSource() == buttons[1])
+		{
+			System.out.println("Button 1");
+		}
+		if(e.getSource() == buttons[2])
+		{
+			System.out.println("Button 2");
+		}
+		if(e.getSource() == buttons[3])
+		{
+			System.out.println("Button 3");
+		}
+		if(e.getSource() == buttons[4])
+		{
+			System.out.println("Button 4");
+		}
+		if(e.getSource() == buttons[5])
+		{
+			System.out.println("Button 5");
+		}
+		if(e.getSource() == buttons[6])
+		{
+			System.out.println("Button 6");
+		}
+		if(e.getSource() == buttons[7])
+		{
+			System.out.println("Button 7");
+		}
+		if(e.getSource() == buttons[8])
+		{
+			System.out.println("Button 8");
+		}
+		if(e.getSource() == buttons[9])
+		{
+			System.out.println("Button 9");
+		}
+		if(e.getSource() == buttons[10])
+		{
+			System.out.println("Button 10");
+		}
+		if(e.getSource() == buttons[11])
+		{
+			System.out.println("Button 11");
+		}
 	}
 	public static void main(String[] args)
 	{
